cd2b8dd4e495899efbf8a497100fbcfc56b80816
Yang <yangnan@arizona.edu>
2025-09-24 14:26:37 -0700

    Update Extracted_10shot.py

--- baselines/Report-task/apple/Extracted_10shot.py
+++ baselines/Report-task/apple/Extracted_10shot.py
@@ -23,7 +23,7 @@ random.seed(args.seed)
 np.random.seed(args.seed)
 
 client = OpenAI(
-    api_key = 'REMOVEDproj-bmA7_VLMa0F2_SpzaPNnY1z8rCUig53Rj0FE64TvGEkRKj3B3b_MoJaAjbvyN3hrqDgTNYUHPRT3BlbkFJqHdwiZZa50YKCrUN5C2V9XL4MsK_Sw636h2QevSYyi5nxHM0CkYUYMRblzu9w_3YfjqHB0xd0A'
+    api_key = ''
 )
 pdf_path = "fruits/USAPPLE_OutlookReport_2024.pdf"
 
